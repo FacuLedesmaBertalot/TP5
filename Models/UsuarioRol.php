5f835509f456022bef876dde01c2f5a34de8c6b0
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-10-31 15:52:00 -0300

    Añadido UsuarioRol.php

--- Models/UsuarioRol.php
+++ Models/UsuarioRol.php
@@ -1,155 +1,60 @@
 <?php
 
-require_once 'DataBase.php';
+namespace Model;
 
-class Rol {
+class UsuarioRol extends ActiveRecord {
 
-    // Atributos
-    private $idRol;
-    private $rodescripcion;
-    private $mensajeOperacion;
+    protected static $tabla = 'usuarioRol';
+    protected static $columnasDB = ['idUsuario', 'idRol'];
 
-    public function __construct()
+
+    public $idUsuario;
+    public $idRol;
+
+
+    // Construct
+    public function __construct($args = [])
     {
-        $this->idRol = 0;
-        $this->rodescripcion = '';
-        $this->mensajeOperacion = '';
+        $this->idUsuario = $args['idUsuario'] ?? null;
+        $this->idRol = $args['idRol'] ?? null;
+    }
+
+    // Validaciones
+    public function validar() {
+        if(!$this->idUsuario) {
+            self::$alertas['error'][] = 'El ID del Usuario es obligatorio';
+        }
+        if(!$this->idRol) {
+            self::$alertas['error'][] = 'El ID del Rol es obligatorio';
+        }
+        return self::$alertas;
     }
 
 
-    // GETTERS
-    public function getIdRol() {
-        return $this->idRol;
-    }
 
-    public function getRoDescripcion() {
-        return $this->rodescripcion;
-    }
-
-    public function getMensajeOperacion() {
-        return $this->mensajeOperacion;
-    }
-
-    
-    // SETTERS
-    public function setIdRol($idRol) {
-        $this->idRol = $idRol;
-    }
-
-    public function setRoDescripcion($rodescripcion) {
-        $this->rodescripcion = $rodescripcion;
-    }
-
-    public function setMensajeOperacion($mensajeOperacion) {
-        $this->mensajeOperacion = $mensajeOperacion;
-    }
-
-    public function setear($idRol, $rodescripcion) {
-        $this->setIdRol($idRol);
-        $this->setRoDescripcion($rodescripcion);
-    }
+    // MÉTODOS 
 
 
-    // --- MÉTODOS CRUD ---
-
-    public function cargar()
-    {
-        $resp = false;
-        $dataBase = new DataBase();
-        $sql = "SELECT * FROM rol WHERE idrol = ?";
+    // Elimina la relacion entre un usuario y un rol
+    public function eliminarRelacion() {
+        $query = "DELETE FROM " . static::$tabla .
+        "WHERE idUsuario " . self::$db->escape_string($this->idUsuario) .
+        "AND idRol = " . self::$db->escape_string($this->idRol) .
+        "LIMIT 1";
         
-        if ($dataBase->iniciar()) {
-            if ($res = $dataBase->ejecutar($sql, "i", [$this->getIdRol()])) {
-                if ($res > 0) {
-                    $row = $dataBase->registro();
-                    $this->setear($row['idrol'], $row['rodescripcion']);
-                    $resp = true;
-                }
-            }
-        } else {
-            $this->setMensajeOperacion("rol->cargar: " . $dataBase->getError());
-        }
-        return $resp;
+        $resultado = self::$db->query($query);
+        return $resultado;
     }
 
-    public function insertar()
-    {
-        $resp = false;
-        $dataBase = new DataBase();
-        $sql = "INSERT INTO rol(rodescripcion) VALUES (?)";
-        
-        if ($dataBase->iniciar()) {
-            if ($id = $dataBase->ejecutar($sql, "s", [$this->getRoDescripcion()])) {
-                $this->setIdRol($id);
-                $resp = true;
-            } else {
-                $this->setMensajeOperacion("rol->insertar: " . $dataBase->getError());
-            }
-        } else {
-            $this->setMensajeOperacion("rol->insertar: " . $dataBase->getError());
-        }
-        return $resp;
-    }
 
-    public function modificar()
-    {
-        $resp = false;
-        $dataBase = new DataBase();
-        $sql = "UPDATE rol SET rodescripcion = ? WHERE idrol = ?";
-        
-        if ($dataBase->iniciar()) {
-            $params = [$this->getRoDescripcion(), $this->getIdRol()];
-            if ($dataBase->ejecutar($sql, "si", $params)) {
-                $resp = true;
-            } else {
-                $this->setMensajeOperacion("rol->modificar: " . $dataBase->getError());
-            }
-        } else {
-            $this->setMensajeOperacion("rol->modificar: " . $dataBase->getError());
-        }
-        return $resp;
-    }
+    // Busca los roles asociados a un id
+    public static function findRolesPorUsuario($idUsuario) {
+        $query = "SELECT r.*
+                FROM rol r " . 
+                "INNER JOIN usuarioRol ur ON r.idRol = ur.idRol " . 
+                "WHERE ur.idUsuario = {$idUsuario}";
 
-    public function eliminar()
-    {
-        $resp = false;
-        $dataBase = new DataBase();
-        $sql = "DELETE FROM rol WHERE idrol = ?";
-        
-        if ($dataBase->iniciar()) {
-            if ($dataBase->ejecutar($sql, "i", [$this->getIdRol()])) {
-                $resp = true;
-            } else {
-                $this->setMensajeOperacion("rol->eliminar: " . $dataBase->getError());
-            }
-        } else {
-            $this->setMensajeOperacion("rol->eliminar: " . $dataBase->getError());
-        }
-        return $resp;
-    }
-
-    public static function listar($condicion = "")
-    {
-        $arreglo = [];
-        $dataBase = new DataBase();
-        $sql = "SELECT * FROM rol";
-        if ($condicion != "") {
-            $sql .= ' WHERE ' . $condicion;
-        }
-        
-        if ($dataBase->iniciar()) {
-            $res = $dataBase->ejecutar($sql);
-            if ($res > -1) {
-                if ($res > 0) {
-                    while ($row = $dataBase->registro()) {
-                        $obj = new Rol();
-                        $obj->setear($row['idrol'], $row['rodescripcion']);
-                        array_push($arreglo, $obj);
-                    }
-                }
-            }
-        }
-        return $arreglo;
+        return Rol::SQL($query);
     }
 
 
